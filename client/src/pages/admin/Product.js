ca14a3f040e377d93a65ffb1360f3123f4976f39
laurapurcaro-bit <laura.p@doit-intl.com>
2023-02-19 14:50:33 +0100

    products

--- client/src/pages/admin/Product.js
+++ client/src/pages/admin/Product.js
@@ -4,12 +4,15 @@ import AdminMenu from "../../components/nav/AdminMenu";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Select } from "antd";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const { Option } = Select;
 
 export default function AdminProduct() {
   // context
   const [auth] = useAuth();
+  const navigate = useNavigate();
   // state
   // Categories in the database
   const [categories, setCategories] = useState([]);
@@ -35,6 +38,32 @@ export default function AdminProduct() {
     }
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append("photo", photo);
+      formData.append("name", name);
+      formData.append("description", description);
+      formData.append("price", price);
+      formData.append("category", category);
+      formData.append("shipping", shipping);
+      formData.append("quantity", quantity);
+
+      const { data } = await axios.post("/product", formData);
+      if (data?.error) {
+        toast.error(data.error);
+      } else {
+        // Show the newly created category
+        toast.success(`Product ${data.name} created`);
+        navigate("/dashboard/admin/products");
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error("Create product failed. Try again.");
+    }
+  };
+
   return (
     <>
       <Jumbotron
@@ -50,7 +79,7 @@ export default function AdminProduct() {
 
           <div className="col-md-9">
             <div className="p-3 mt-2 mb-2 h4 bg-light">Create Products</div>
-            {photo && (
+            {photo?.size && (
               <div className="text-center">
                 <img
                   src={URL.createObjectURL(photo)}
@@ -72,18 +101,69 @@ export default function AdminProduct() {
                 />
               </label>
             </div>
+            {/* Category name */}
+            <input
+              type="text"
+              className="form-control mb-3 p-2"
+              placeholder="Write a name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            {/* Description */}
+            <textarea
+              type="text"
+              className="form-control mb-3 p-2"
+              placeholder="Write a description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            {/* PRICE */}
+            <input
+              type="number"
+              className="form-control mb-3 p-2"
+              placeholder="Enter price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+            {/* Categories */}
             <Select
               showSearch
               bordered={false}
               size="large"
               className="form-select mb-3"
               placeholder="Choose a category"
-              onChange={(value) => setCategory(value)}
+              onChange={(category) => setCategory(category)}
             >
               {categories?.map((category) => (
                 <Option key={category._id} value={category.name}></Option>
               ))}
             </Select>
+            {/* Quantity */}
+            <input
+              type="number"
+              min="1"
+              className="form-control mb-3 p-2"
+              placeholder="Enter quantity"
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+            />
+            {/* Shipping */}
+            <Select
+              bordered={false}
+              size="large"
+              className="form-select mb-3"
+              placeholder="Choose shipping"
+              onChange={(shipping) => setShipping(shipping)}
+            >
+              <Option value="0">No</Option>
+              <Option value="1">Yes</Option>
+            </Select>
+            <button
+              onClick={handleSubmit}
+              className="btn btn-outline-primary mb-5"
+            >
+              Save
+            </button>
           </div>
         </div>
       </div>
