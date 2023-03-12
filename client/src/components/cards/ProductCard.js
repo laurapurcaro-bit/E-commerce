282634215a820f413e2528d89fac9b076629247f
laurapurcaro-bit <laura.p@doit-intl.com>
2023-03-12 15:29:01 +0100

    product view

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -1,8 +1,15 @@
 import moment from "moment";
 
 export default function ProductCard({ product }) {
+  function productDesc(description) {
+    if (description.length < 60) {
+      return description;
+    } else {
+      return description.substring(0, 60) + "...";
+    }
+  }
   return (
-    <div className="card mb-3">
+    <div className="card mb-3 hoverable">
       <img
         src={`${process.env.REACT_APP_API}/product/photo/${product._id}`}
         alt={product.name}
@@ -11,9 +18,26 @@ export default function ProductCard({ product }) {
         width="230px"
         style={{ objectFit: "cover" }}
       />
-      <p>{product.name}</p>
-      <p>{moment(product.createdAt).fromNow()}</p>
-      <p>{product.sold} sold</p>
+      <div className="card-body">
+        <h5>{product?.name}</h5>
+        <p className="card-text">{productDesc(product?.description)}</p>
+      </div>
+      <div className="d-flex justify-content-between">
+        <button
+          className="btn btn-primary col card-button-footer"
+          style={{ borderBottomLeftRadius: "5px" }}
+        >
+          View Product
+        </button>
+        <button
+          className="btn btn-outline-primary col card-button-footer"
+          style={{ borderBottomRightRadius: "5px" }}
+        >
+          Add to Cart
+        </button>
+      </div>
+      {/* <p>{moment(product?.createdAt).fromNow()}</p>
+      <p>{product?.sold} sold</p> */}
     </div>
   );
 }
